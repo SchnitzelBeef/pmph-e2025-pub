525d3729832212b3ef544d061dbbb225c998be90
Christian Nielsen <christiannhn@hotmail.com>
2025-09-22 16:11:33 +0200

    task 5 closer

--- weeklies/assignment-2/w2-code-handin/w2-code-handin/cuda/spmv_mul_kernels.cuh
+++ weeklies/assignment-2/w2-code-handin/w2-code-handin/cuda/spmv_mul_kernels.cuh
@@ -30,51 +30,33 @@ __global__ void replicate0(int tot_size, char* flags_d) {
 __global__ void mkFlags(int mat_rows, int* mat_shp_sc_d, char* flags_d) {
     const unsigned int gid_x = blockIdx.x * blockDim.x + threadIdx.x;
     // Mat rows must be equal to the size of the mat_shp_sc_d array
-    
-    unsigned int flag = 0;
-    // Exlusive scan 
-    if (gid_x == 1) {
-        flag = mat_shp_sc_d[0];
-    } 
-    else {
-        flag = mat_shp_sc_d[gid_x-1] + mat_shp_sc_d[gid_x];
-        // mat_shp_sc_d[gid_x] = mat_shp_sc_d[gid_x-1] + mat_shp_sc_d[gid_x] ;
-    } 
-    if (flag < mat_rows) {
-        flags_d[flag] = 1;
-    }  
 
+    flags_d[0] = 1;
+    if (gid_x < mat_rows - 1) {
+        const unsigned int flag = mat_shp_sc_d[gid_x];
+        flags_d[flag] = 1;
+    }
     // Length is equal to the size of flags_d since it is based on tot_size
-    // if (mat_shp_sc_d[gid_x] < mat_rows) {
-    //     flags_d[mat_shp_sc_d[gid_x]] = 1;
-    // }
 }
 
 
 __global__ void mult_pairs(int* mat_inds, float* mat_vals, float* vct, int tot_size, float* tmp_pairs) {
     const unsigned int gid_x = blockIdx.x * blockDim.x + threadIdx.x;
-
-    // map (\(i, v) -> vct[i] * v) mat_val 
-
     if (gid_x < tot_size) {
         const unsigned int i = mat_inds[gid_x];
         const unsigned int v = mat_vals[gid_x];
         // We use the temorary array since the same index are accessed concurrently in the vector
-        tmp_pairs[gid_x] = vct[i] * v ; 
+        tmp_pairs[gid_x] = vct[i] * v; 
     }
 }
 
 
 __global__ void select_last_in_sgm(int mat_rows, int* mat_shp_sc_d, float* tmp_scan, float* res_vct_d) {
     const unsigned int gid_x = blockIdx.x * blockDim.x + threadIdx.x;
-    
-    // map (\i -> tmp_scan[i-1]) shp_sc
-    
     if (gid_x < mat_rows) {
         const unsigned int i = mat_shp_sc_d[gid_x];
         res_vct_d[gid_x] = tmp_scan[i-1];
     }
-
 }
 
 #endif // SPMV_MUL_KERNELS
