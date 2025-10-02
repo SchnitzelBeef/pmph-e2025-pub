9d8b487593db127d54b0999e3a019f81fa0f96d9
SchnitzelBeef <jlw218@alumni.ku.dk>
2025-10-02 16:35:27 +0200

    task 6 attempt

--- weeklies/assignment-3-4/w3-code-handin/w3-code-handin/batch-mmm/kernels.cu.h
+++ weeklies/assignment-3-4/w3-code-handin/w3-code-handin/batch-mmm/kernels.cu.h
@@ -99,5 +99,22 @@ void bmmmTiledKer ( ElTp* A,      ElTp* B, char* X_tr,   ElTp* Y
    * hold in global memory, i.e., A, B, X_tr, Y.
    ***********************************************/
 
+  for (int q = 0; q < N; q++) {
+    ElTp ab = A[j1 * N + q] * B[q * K + j2];
+    int  i = ii + flat_thid;
+    char x = (flat_thid < T && i < M) ? X_tr[q * M + i] : 0;
+    Xsh_tr[flat_thid];
+
+    #pragma unroll
+    for (int i_r = 0; i_r < T; i_r++) {
+      float v = (Xsh_tr[i_r] != 0) ? 1 : 0;
+      acc[i_r] += ab * v;
+    }
+  }
+
+  for (int i_r = 0; i_r < T; i_r++) {
+    if (ii + i_r < M) Y[(ii + i_r) * K + j1 * K + j2] = acc[i_r]; 
+  }
+
 }
 #endif
